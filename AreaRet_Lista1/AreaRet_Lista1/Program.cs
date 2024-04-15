cdb8aa185a354145287a20fbe1202f80d20d1144
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-15 20:48:02 -0300

    Add files via upload

---
+++ AreaRet_Lista1/AreaRet_Lista1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaRet_Lista1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int area;
+            string aux;
+
+                Console.Write("Digite a base do retângulo ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura do retângulo ");
+            b = int.Parse(Console.ReadLine());
+
+            area = a * b;
+
+                Console.Write("A area do retângulo é: ");
+            Console.WriteLine(area);
+
+
+
+            
+            
+
+            
+           
+
+
+
+
+        }
+    }
+}
