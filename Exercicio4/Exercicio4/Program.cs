826de7e2c746bc208f4b4992787183b8c05c9721
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-16 20:18:44 -0300

    Add files via upload

---
+++ Exercicio4/Exercicio4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            int area;
+            string aux;
+
+            Console.Write("Digite a base o triângulo: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite a altura o triângulo: ");
+            b = int.Parse(Console.ReadLine());
+            area = (a * b) / 2;
+
+            Console.WriteLine("A area do triângulo é: ");
+            Console.WriteLine(area);
+        }
+    }
+}
