9f743e3ef7a34dee8613aa8758e6c213795da65a
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-15 21:10:15 -0300

    Add files via upload

---
+++ Exercicio2/Exercicio2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            int area;
+
+       
+            Console.WriteLine("Digite o valor da aresta do quadrado");
+            l = int .Parse(Console.ReadLine());
+
+            area = l * l;
+
+            Console.WriteLine("O valor da área do quadrado é: ");
+
+            Console.WriteLine(area);
+
+        }
+    }
+}
