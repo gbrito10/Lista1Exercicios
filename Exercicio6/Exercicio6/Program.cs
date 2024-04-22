3a21de0a477fa5f26ec0dfcffa4674bc4037e6c4
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-22 20:55:40 -0300

    Add files via upload

---
+++ Exercicio6/Exercicio6/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            int b;
+            double media;
+
+            Console.Write("Digite o primeiro valor");
+            a = int.Parse(Console.ReadLine());
+
+            Console.Write("Digite o segundo valor");
+            b = int.Parse(Console.ReadLine());
+
+            media = Math.Sqrt(a * b);
+
+            Console.Write("A média geométrica é: {0}", media);
+        }
+    }
+}
