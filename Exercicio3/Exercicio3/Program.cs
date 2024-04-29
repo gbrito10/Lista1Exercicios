13363cacb0e4f18f851d84ecf5328e74e2d334f5
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-29 20:51:09 -0300

    Add files via upload

---
+++ Exercicio3/Exercicio3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o valor da diagonal do quadrado: ");
+            double diagonal = Convert.ToDouble(Console.ReadLine());
+
+            double lado = diagonal / Math.Sqrt(2);
+
+            double area = lado * lado;
+
+            Console.WriteLine($"A área do quadrado é: {area}");
+        }
+    }
+}
