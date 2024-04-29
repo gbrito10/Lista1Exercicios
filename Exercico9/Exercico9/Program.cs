1af64b484a89c6fc3f106d1f601acf2dcbb6db5a
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-29 20:26:39 -0300

    Add files via upload

---
+++ Exercico9/Exercico9/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercico9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite o diâmetro do círculo: ");
+            double diametro = Convert.ToDouble(Console.ReadLine());
+
+            double raio = diametro / 2;
+            double area = Math.PI * Math.Pow(raio, 2);
+
+            Console.WriteLine($"A área do círculo é: {area}");
+        }
+    }
+}
