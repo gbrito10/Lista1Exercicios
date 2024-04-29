0af819e9c514ee7aaffc7c8b83933201366faacf
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-29 20:11:36 -0300

    Add files via upload

---
+++ Exercicio8/Exercicio8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Conversor de temperatura de Celsius para Fahrenheit");
+            Console.Write("Digite a temperatura em graus Celsius: ");
+
+            double celsius = Convert.ToDouble(Console.ReadLine());
+
+            double fahrenheit = (celsius * 9 / 5) + 32;
+
+            Console.WriteLine($"A temperatura em Fahrenheit é: {fahrenheit:F2} °F");
+
+
+        }
+    }
+}
