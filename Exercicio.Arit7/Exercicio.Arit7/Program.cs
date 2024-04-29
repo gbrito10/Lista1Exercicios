52eab753d1076b6c78b039d51fa8ae58244bd145
gbrito10 <165733920+gbrito10@users.noreply.github.com>
2024-04-29 20:01:46 -0300

    Add files via upload

---
+++ Exercicio.Arit7/Exercicio.Arit7/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercicio.Arit7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double MilhaMaritimaParaMetro = 1852;
+            const double MetroParaQuilometro = 0.001;
+
+            Console.WriteLine("Digite a quantidade de milhas marítimas: ");
+            double milhasMaritimas = Convert.ToDouble(Console.ReadLine());
+
+            double quilometros = (milhasMaritimas * MilhaMaritimaParaMetro) * MetroParaQuilometro;
+
+            Console.WriteLine($"{milhasMaritimas} milhas marítimas equivalem a {quilometros} quilômetros");
+
+            
+        }
+    }
+}
